f57938fb471a958869c6411d8b0b5d4727ff7dc5
AbdulFatah1309 <af434613@gmail.com>
2025-09-24 08:48:05 +0700

    model application

--- internal/model/application_model.go
+++ internal/model/application_model.go
@@ -1,5 +1,17 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Application struct {
-	
+	ID        uint64 `gorm:"primaryKey;autoIncrement"`
+	Name      string `gorm:"size:100;unique;not null;comment:Nama aplikasi"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Aplication []LogHistory `gorm:"foreignKey:ApplicationID" json:"Aplication"`
 }
